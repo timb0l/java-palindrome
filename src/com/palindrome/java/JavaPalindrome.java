5aa21f63b331f29d1769aba45a46dd8d5f6a86b2
Jam Timbol <jam.timbol00@gmail.com>
2023-07-17 14:48:32 +0200

    Palindrome Done

--- src/com/palindrome/java/JavaPalindrome.java
+++ src/com/palindrome/java/JavaPalindrome.java
@@ -1,4 +1,25 @@
 package com.palindrome.java;
 
+import java.util.Scanner;
+
 public class JavaPalindrome {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+            Boolean a = true;
+            char[] word;
+        System.out.println("Type word..");
+
+        word = scan.next().toCharArray();
+        int b = word.length -1;
+
+        for (int c = 0; c >= word.length; c ++){
+            if (!(word[c] == word[b])){
+                a = false;
+            } b--;
+        }if (a == false){
+            System.out.println(word.length + "is not palindrome word.");
+        }else
+            System.out.println(word.length + "is palindrome word.");
+        scan.close();
+    }
 }
